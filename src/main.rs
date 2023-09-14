6d9e4dfa51dac92ebacd81bea7aa3189f4b12acd
flaribbit <1149761294@qq.com>
2023-09-14 08:55:05 +0800

    tokenizer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+mod model;
+use tokenizers::tokenizer::{Result, Tokenizer};
+
+fn main() -> Result<()> {
+    let mut tokenizer = Tokenizer::from_file("./clip/tokenizer.json")?;
+    let encoding = tokenizer.encode("Hey there!", true)?;
+    println!("{:?}", encoding.get_ids());
+    Ok(())
 }
