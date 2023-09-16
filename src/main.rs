da049e19542b76b4783179f67c25d16db26d6afe
flaribbit <1149761294@qq.com>
2023-09-16 15:27:55 +0800

    支持搜图啦

--- src/main.rs
+++ src/main.rs
@@ -44,10 +44,30 @@ fn add_image_feature(
     let img = load_image224(path)
         .expect("failed to load image")
         .unsqueeze(0)?;
-    let output: Vec<f32> = model.forward(&img)?.unsqueeze(0)?.to_vec1()?;
+    let output: Vec<f32> = model.forward(&img)?.squeeze(0)?.to_vec1()?;
     let output = normalize(&output);
     database.insert(path.to_string(), output);
     Ok(())
+}
+
+fn get_images(path: &str) -> Vec<String> {
+    let mut result = Vec::new();
+    fn recurse(path: &str, result: &mut Vec<String>) {
+        for entry in std::fs::read_dir(path).expect("failed to read dir") {
+            let entry = entry.expect("failed to read entry");
+            let path = entry.path();
+            if path.is_dir() {
+                recurse(&path.to_string_lossy(), result);
+            } else {
+                let path = path.to_string_lossy();
+                if path.ends_with(".jpg") || path.ends_with(".png") || path.ends_with(".jpeg") {
+                    result.push(path.to_string());
+                }
+            }
+        }
+    }
+    recurse(path, &mut result);
+    result
 }
 
 fn find_image<'a>(
@@ -62,7 +82,7 @@ fn find_image<'a>(
     text_ids[..encoding_len].copy_from_slice(&encoding.get_ids()[..encoding_len]);
     let feature: Vec<f32> = model
         .forward(&Tensor::from_vec(text_ids.to_vec(), (1, 77), &Device::Cpu)?)?
-        .unsqueeze(0)?
+        .squeeze(0)?
         .to_vec1()?;
     let feature = normalize(&feature);
     let mut result = Vec::new();
@@ -72,6 +92,31 @@ fn find_image<'a>(
     }
     result.sort_by(|(_, a), (_, b)| b.partial_cmp(a).unwrap());
     Ok(result)
+}
+
+fn command_add_image(database: &mut Database, path: &str, model: &model::ClipVisionTransformer) {
+    let images = get_images(path);
+    let len = images.len();
+    for (i, image) in images.iter().enumerate() {
+        if database.contains_key(image) {
+            println!("skipping {}/{} {}", i + 1, len, image);
+            continue;
+        }
+        println!("processing {}/{} {}", i + 1, len, image);
+        add_image_feature(database, model, &image).expect("failed to add image");
+    }
+}
+
+fn command_find_image(
+    database: &mut Database,
+    model: &model::ClipTextTransformer,
+    tokenizer: &Tokenizer,
+    text: &str,
+) {
+    let result = find_image(database, model, tokenizer, text).expect("failed to find image");
+    for (i, (path, similarity)) in result.iter().enumerate() {
+        println!("{:2} {:.4} {}", i, similarity, path);
+    }
 }
 
 fn normalize(x: &[f32]) -> Vec<f32> {
@@ -92,6 +137,32 @@ fn cos_sim(e_i: Tensor, e_j: Tensor) -> Result<f32> {
 
 fn main() -> Result<()> {
     let mut database = load_database();
+    let arg1 = std::env::args().nth(1);
+    let arg2 = std::env::args().nth(2);
+    if arg1 == Some("add".to_string()) && arg2.is_some() {
+        let weights =
+            unsafe { candle_core::safetensors::MmapedFile::new("clip/model.safetensors")? };
+        let weights = weights.deserialize()?;
+        let vb = VarBuilder::from_safetensors(vec![weights], DType::F32, &Device::Cpu);
+        let model = model::ClipVisionTransformer::new(vb, &model::Config::vision())?;
+        command_add_image(&mut database, &arg2.unwrap(), &model);
+        save_database(&database);
+        return Ok(());
+    } else if arg1 == Some("find".to_string()) && arg2.is_some() {
+        let weights =
+            unsafe { candle_core::safetensors::MmapedFile::new("clip/model.safetensors")? };
+        let weights = weights.deserialize()?;
+        let vb = VarBuilder::from_safetensors(vec![weights], DType::F32, &Device::Cpu);
+        let model = model::ClipTextTransformer::new(vb, &model::Config::clip())?;
+        let tokenizer = Tokenizer::from_file("./clip/tokenizer.json")?;
+        command_find_image(&mut database, &model, &tokenizer, &arg2.unwrap());
+        return Ok(());
+    }
+    println!("usage: clip add <path> | clip find <text>");
+    Ok(())
+}
+
+fn main2() -> Result<()> {
     // get args
     let image_path = std::env::args()
         .nth(1)
@@ -126,4 +197,14 @@ fn main() -> Result<()> {
     println!("similarity = {}", similarity);
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_get_images() {
+        let images = get_images(".");
+        println!("{:?}", images);
+    }
 }
