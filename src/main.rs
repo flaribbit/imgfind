cb57d38a9714c2617f2b4bffccf6b093e28cc69d
flaribbit <1149761294@qq.com>
2023-09-16 10:59:58 +0800

    存储特征和搜图函数

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 mod model;
+use std::collections::BTreeMap;
+
 use candle_core::Module;
 use candle_core::{DType, Device, Tensor};
 use candle_nn::VarBuilder;
@@ -19,6 +21,68 @@ pub fn load_image224<P: AsRef<std::path::Path>>(p: P) -> candle_core::Result<Ten
         .broadcast_div(&std)
 }
 
+type Embedding = Vec<f32>;
+type Database = BTreeMap<String, Embedding>;
+
+fn load_database() -> Database {
+    match std::fs::File::open("database.bin") {
+        Ok(file) => rmp_serde::from_read(file).expect("failed to read database.bin"),
+        Err(_) => BTreeMap::new(),
+    }
+}
+
+fn save_database(database: &Database) {
+    let mut file = std::fs::File::create("database.bin").expect("failed to create database.bin");
+    rmp_serde::encode::write(&mut file, database).expect("failed to write database.bin");
+}
+
+fn add_image_feature(
+    database: &mut Database,
+    model: &model::ClipVisionTransformer,
+    path: &str,
+) -> Result<()> {
+    let img = load_image224(path)
+        .expect("failed to load image")
+        .unsqueeze(0)?;
+    let output: Vec<f32> = model.forward(&img)?.unsqueeze(0)?.to_vec1()?;
+    let output = normalize(&output);
+    database.insert(path.to_string(), output);
+    Ok(())
+}
+
+fn find_image(
+    database: &mut Database,
+    model: &model::ClipTextTransformer,
+    tokenizer: &Tokenizer,
+    text: &str,
+) -> Result<Vec<(String, f32)>> {
+    let mut text_ids = [0u32; 77];
+    let encoding = tokenizer.encode(text, true)?;
+    let encoding_len = encoding.get_ids().len().min(77);
+    text_ids[..encoding_len].copy_from_slice(&encoding.get_ids()[..encoding_len]);
+    let feature: Vec<f32> = model
+        .forward(&Tensor::from_vec(text_ids.to_vec(), (1, 77), &Device::Cpu)?)?
+        .unsqueeze(0)?
+        .to_vec1()?;
+    let feature = normalize(&feature);
+    let mut result: Vec<(String, f32)> = Vec::new();
+    for (path, embedding) in database.iter() {
+        let similarity = dot_product(embedding, &feature);
+        result.push((path.clone(), similarity));
+    }
+    result.sort_by(|(_, a), (_, b)| b.partial_cmp(a).unwrap());
+    Ok(result)
+}
+
+fn normalize(x: &[f32]) -> Vec<f32> {
+    let sum: f32 = x.iter().map(|x| x * x).sum::<f32>().sqrt();
+    x.iter().map(|x| x / sum).collect()
+}
+
+fn dot_product(x: &[f32], y: &[f32]) -> f32 {
+    x.iter().zip(y).map(|(x, y)| x * y).sum()
+}
+
 fn cos_sim(e_i: Tensor, e_j: Tensor) -> Result<f32> {
     let sum_ij = (&e_i * &e_j)?.sum_all()?.to_scalar::<f32>()?;
     let sum_i2 = (&e_i * &e_i)?.sum_all()?.to_scalar::<f32>()?;
@@ -27,6 +91,7 @@ fn cos_sim(e_i: Tensor, e_j: Tensor) -> Result<f32> {
 }
 
 fn main() -> Result<()> {
+    let mut database = load_database();
     // get args
     let image_path = std::env::args()
         .nth(1)
